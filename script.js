cf7c2793766f025793964e5120932c07660073a2
frostyausty <priceaustin62@gmail.com>
2020-09-19 10:52:52 -0500

    added password length prompt

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Assignment code here
+var generatePassword = function() {
+  var passwordLength = window.prompt("How many characters do you want your password to be? Enter a number between 8 and 128")
+  while (passwordLength < 8 || passwordLength > 128){
+    passwordLength = window.prompt("Enter a password between 8 and 128 characters")
 
+    while 
+    
+  }
+  return passwordLength
+  
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +26,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
