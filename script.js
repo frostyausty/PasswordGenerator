ce11c209cb0150caa6872f270689588ecfc28a97
frostyausty <priceaustin62@gmail.com>
2020-09-19 11:54:49 -0500

    added conditional prompts for upper/lower case, numbers and special character

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // Assignment code here
+var characterOption = function() {
+  var characterLower = window.confirm("Do you want your password to have lower case characters?");
+  var characterUpper = window.confirm("Do you want your password to have upper case characters?");
+  var characterNumbers = window.confirm("Do you want to numbers in your password?");
+  var characterSpecial = window.confirm("Do you want your password to have special characters?");
+    if (characterLower === false && characterUpper === false && characterNumbers === false && characterSpecial === false){
+      window.alert("Please select at least one option.");
+      characterOption();
+    }
+}
+
 var generatePassword = function() {
   var passwordLength = window.prompt("How many characters do you want your password to be? Enter a number between 8 and 128")
-  while (passwordLength < 8 || passwordLength > 128){
+  while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = window.prompt("Enter a password between 8 and 128 characters")
-
-    while 
-    
+ 
   }
+  characterOption();
   return passwordLength
   
 };
